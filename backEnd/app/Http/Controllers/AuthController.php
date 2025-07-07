38521b0aea412a682b42a13c545d7efde0cc1d73
gaji asif <asif01665@yahoo.com>
2025-07-07 11:59:55 +0300

    update form request to base request

--- backEnd/app/Http/Controllers/AuthController.php
+++ backEnd/app/Http/Controllers/AuthController.php
@@ -4,21 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
+use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
+    public function register(RegisterRequest $request){
         // $request->validate([
         //     'first_name'=>'required|string|max:255',
         //     'last_name' => 'required|string|max:255',
         //     'email'=>'required|string|email|max:255|unique:users',
         //     'password'=>'required|string|min:8|confirmed'
         // ]);
+       
+
+        $attr = $request->validated();
 
         return response()->json([
             'message' => 'success'
-        ]);
+        ], 201);
 
         
     }
+    
 }

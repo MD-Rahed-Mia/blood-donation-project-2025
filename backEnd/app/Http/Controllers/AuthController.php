aae9af14ff737e92d2de38edea84346b7303dcb6
gaji asif <asif01665@yahoo.com>
2025-07-10 12:18:19 +0300

    added Login Api Endpoint

--- backEnd/app/Http/Controllers/AuthController.php
+++ backEnd/app/Http/Controllers/AuthController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -33,5 +35,36 @@ class AuthController extends Controller
         // ]);
         
        }
+
+      /*  login API
+
+       @return @Illuminate\Http\Response 
+       */
+
+    public function login(LoginRequest $request){
+        $attr = $request->validated();
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+        if(Auth::attempt($data)){
+            $user = Auth::user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return response()->json([
+                'success' => true,
+                'message' => 'User logged in Successfully',
+                'data' => $user,
+                'token' => $token,
+                'token_type' => 'Bearer'
+            ]);
+        }
+        else{
+            return response()->json([
+                'success' => false,
+                'message' => 'unathorized',
+                'data' => []
+            ]);
+        }
+    }
     
 }
--- backEnd/routes/api.php
+++ backEnd/routes/api.php
@@ -14,4 +14,4 @@ use App\Http\Controllers\AuthController;
 // });
 
 Route::post('/register', [AuthController::class, 'register']);
-//Route::post('login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
